9dbe35b91e12063ca51589ff42420e460d146c88
chanisxx <torreschanis@gmail.com>
2025-06-30 03:48:03 -0400

    included Astro framework

--- website/assets/js/counter.js
+++
@@ -1,8 +0,0 @@
-const counter = document.querySelector('.counter-number');
-
-async function updateViewCount() {
-    let response = await fetch('https://xgqyiw2bv24hgpliuimoeadhhq0wxmnn.lambda-url.us-east-1.on.aws/');
-    let data = await response.json();
-    counter.innerHTML = `${data}`;
-}
-updateViewCount();
